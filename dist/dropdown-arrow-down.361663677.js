7181330c8e3c03d20f050e67ded1b0c55108ad93
Alan Reid <alan.reid@businessandtrade.gov.uk>
2025-04-30 12:44:56 +0100

    Update to karma and wdio config, and GH int tests

---
+++ dist/dropdown-arrow-down.361663677.js
@@ -0,0 +1,22 @@
+'use strict';
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function webpackUniversalModuleDefinition (root, factory) {
+  if (typeof exports === 'object' && typeof module === 'object') { module.exports = factory() } else if (typeof define === 'function' && define.amd) { define([], factory) } else if (typeof exports === 'object') { exports.accessibleAutocomplete = factory() } else { root.accessibleAutocomplete = factory() }
+})(self, function () {
+  return (self.webpackChunkaccessibleAutocomplete = self.webpackChunkaccessibleAutocomplete || []).push([['dropdown-arrow-down.361663677'], {},
+    /******/ function (__webpack_require__) { // webpackRuntimeModules
+      /******/ const __webpack_exec__ = function (moduleId) { return __webpack_require__(__webpack_require__.s = moduleId) }
+      /******/ __webpack_require__.O(0, ['commons'], function () { return __webpack_exec__('../test/functional/dropdown-arrow-down.js') })
+      /******/ let __webpack_exports__ = __webpack_require__.O()
+      /******/ __webpack_exports__ = __webpack_exports__.default
+      /******/ return __webpack_exports__
+      /******/ }
+  ])
+})
