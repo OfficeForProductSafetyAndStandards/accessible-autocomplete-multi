8246fb4908d35bfd5d51a82ebc094456b0bf85cb
Alan Reid <alan.reid@businessandtrade.gov.uk>
2025-05-02 13:40:51 +0100

    Update to the Karma config and  update npm verion

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,11 +1,10 @@
 require('@babel/register')({
   cwd: require('path').resolve(__dirname, '../')
 })
-
 const puppeteer = require('puppeteer')
 const baseWebpackConfig = require('../webpack.config.babel.js')[0]
 const webpack = require('webpack')
-
+const path = require('path')
 process.env.CHROME_BIN = puppeteer.executablePath()
 
 module.exports = function (config) {
@@ -13,28 +12,37 @@ module.exports = function (config) {
     basePath: '../',
     frameworks: ['mocha', 'chai-sinon'],
     reporters: ['mocha'],
-
     browsers: ['ChromeHeadlessNoSandbox'],
-
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
       }
     },
-
     files: [
       'test/functional/**/*.js'
     ],
-
     preprocessors: {
       'test/**/*.js': ['webpack'],
       'src/**/*.js': ['webpack'],
       '**/*.js': ['sourcemap']
     },
-
     webpack: {
       ...baseWebpackConfig,
+      module: {
+        rules: [
+          ...(baseWebpackConfig.module?.rules || []),
+          {
+            test: /\.js$/,
+            include: [
+              path.resolve(__dirname, '../node_modules/chai')
+            ],
+            use: {
+              loader: 'babel-loader'
+            }
+          }
+        ]
+      },
       plugins: [
         ...(baseWebpackConfig.plugins || []),
         new webpack.DefinePlugin({
@@ -42,7 +50,6 @@ module.exports = function (config) {
         })
       ]
     },
-
     webpackMiddleware: {
       logLevel: 'error',
       stats: 'errors-only'
